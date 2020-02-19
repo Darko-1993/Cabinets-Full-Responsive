86032af2941dfe929e3ed5f6c369a407b3386395
Darko-1993 <58879836+Darko-1993@users.noreply.github.com>
2020-02-19 21:59:05 +0100

    new

---
+++ js/javaScript.js
@@ -0,0 +1,44 @@
+function runSite(){
+	
+    var btn = $('#button');
+
+    $(window).scroll(function() {
+      if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+      } else {
+        btn.removeClass('show');
+      }
+    });
+
+    btn.on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({scrollTop:0}, '300');
+    });
+	
+	$(function () {
+			$(window).on('scroll', function () {
+				if ( $(window).scrollTop() > 10 ) {
+					$('.navbar').addClass('active');
+				} else {
+					$('.navbar').removeClass('active');
+				}
+			});
+		})
+	
+}     
+
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:1,
+        itemsDesktop:[1000,1],
+        itemsDesktopSmall:[990,1],
+        itemsTablet:[768,1],
+        itemsMobile:[650,1],
+        pagination:true,
+        navigation:false,
+        autoPlay:true
+    });
+});
+	
+    
+
